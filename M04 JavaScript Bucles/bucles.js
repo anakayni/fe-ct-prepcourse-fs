ca1d8317a9dfeef23abeaae3c52eb6ca3525c539
anakayni <anakayni@gmail.com>
2023-10-26 10:24:47 -0600

    Homework mdulo 4

---
+++ M04 JavaScript Bucles/bucles.js
@@ -0,0 +1,33 @@
+var suma = 0;
+//suma = suma +1;
+//suma = suma +2;
+//suma = suma +4;
+
+//console.log(suma)
+
+//BUCLE FOR
+
+//for (var i = 0; i < 10; i ++){
+//    suma = suma +1;
+//    console.log(suma);
+//}
+
+//for (var i = 0; i < 5; i ++){
+//    suma = suma + i;
+//    console.log('Variable de iteracion: ', i);
+//}
+
+//console.log('Variable suma: ', suma);
+
+//BUCLE WHILE
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
+
+//for of
+var comidas =["pizza", "cafe", "empanada"];
+
+for (comida of comidas){
+    console.log(comida);
+}
